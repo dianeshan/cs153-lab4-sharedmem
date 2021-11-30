3be75b1b11a676e4425701223ac9d1d3f9ef2f62
Brent Sakihara <bsaki001@sledge.cs.ucr.edu>
2021-11-30 10:10:24 -0800

    made shm_open and close

--- shm.c
+++ shm.c
@@ -7,6 +7,8 @@
 #include "proc.h"
 #include "spinlock.h"
 
+#define SHM_BASE KERNBASE - (64 * PGSIZE)
+
 struct {
   struct spinlock lock;
   struct shm_page {
@@ -33,32 +35,64 @@ int shm_open(int id, char **pointer) {
 //you write this
 
 //acquire lock
+acquire(&(shm_table.lock));
 
 //check if shared page exists, if not kalloc() a new one
 //might need to use memset()
+int i;
+int flag = 0;
+uint sz = PGROUNDUP(myproc()->sz); //or use SHM_BASE + PGSIZE*i
+for(i = 0; i < 64; i++){
+  if(shm_table.shm_pages[i].id == id){
+    mappages(myproc()->pgdir, (char*)(sz), PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W|PTE_U);
+    shm_table.shm_pages[i].refcnt++;
+    *pointer=(char *)(sz);
+    //myproc()->sz = PGROUNDUP(myproc()->sz);
+    flag = 1;
+    break;
+  }
+}
 
-//increment reference count
-
-//mappages() use this to map from virtual to physical memory
-
-//set pointer = VA
+if(flag == 0){
+  for(i = 0; i < 64; i++){
+    if(shm_table.shm_pages[i].id == 0 && shm_table.shm_pages[i].frame == 0 && shm_table.shm_pages[i].refcnt == 0){ //finds an empty page
+      shm_table.shm_pages[i].id = id;
+      shm_table.shm_pages[i].frame = kalloc();
+      memset(shm_table.shm_pages[i].frame, 0, PGSIZE);
+      mappages(myproc()->pgdir, (char*)(sz), PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W|PTE_U);
+      shm_table.shm_pages[i].refcnt = 1;
+      *pointer=(char *)(sz);
+      //myproc()->sz = PGROUNDUP(myproc()->sz);
+    break;
+  }
+  }
+}
 
 //release lock
+release(&(shm_table.lock));
 
-
-return 0; //added to remove compiler warning -- you should decide what to return
+return 0;
 }
 
 
 int shm_close(int id) {
-//you write this too!
 
-//don't forget to use locks here too
+acquire(&(shm_table.lock));
+int i;
+for(i = 0; i < 64; i++){
+  if(shm_table.shm_pages[i].id == id){
+    //decrement reference count
+    shm_table.shm_pages[i].refcnt--;
+    //if after decrementing, refcnt = 0, clear page 
+    if(shm_table.shm_pages[i].refcnt == 0){
+      shm_table.shm_pages[i].id = 0;
+      shm_table.shm_pages[i].frame = 0;
+    }
+    break;
+  }
+}
 
-//decrement reference count
-
-//if after decrementing, refcnt = 0, clear page 
-
+release(&(shm_table.lock));
 
 return 0; //added to remove compiler warning -- you should decide what to return
 }
